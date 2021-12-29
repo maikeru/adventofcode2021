4248e754da6ce31e0f9e0292d9cd4e719da3da68
P. Michael Holland <maikeru.horando@gmail.com>
2021-12-29 17:38:10 +0100

    Day 6 part 2

--- day_06_1/main.go
+++ day_06_1/main.go
@@ -7,37 +7,51 @@ import (
 	"github.com/maikeru/adventofcode2021/utils"
 )
 
-func SimulateDay(fishCollection *[]int) *[]int {
-	newFish := make([]int, 0)
-	for i := range *fishCollection {
-		fish := (*fishCollection)[i]
-		if fish-1 < 0 {
-			(*fishCollection)[i] = 6
-			newFish = append(newFish, 8)
-		} else {
-			(*fishCollection)[i] = fish - 1
-		}
+// Credit to u/Evargalo for the idea of using a map like this
+// https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hq75dbm/?utm_source=share&utm_medium=web2x&context=3
+func SimulateDay(fishMap map[int]int) map[int]int {
+	motherFishCount := fishMap[0]
+	// Decrease time left for all fish not giving birth
+	for i := 1; i < 9; i++ {
+		fishMap[i-1] = fishMap[i]
 	}
+	// roll birthing fish around to 6 days left
+	fishMap[6] += motherFishCount
 
-	// New fish should appear at the end
-	result := append(*fishCollection, newFish...)
-	return &result
+	// birth new fish equivalent to number of mother fish
+	fishMap[8] = motherFishCount
+	return fishMap
+}
+
+func createFishMap(initialFish []int) map[int]int {
+	fishMap := make(map[int]int, 8)
+	for i := range initialFish {
+		currentFish := initialFish[i]
+		fishMap[currentFish] = fishMap[currentFish] + 1
+	}
+	return fishMap
 }
 
 func GetFishCount(initialFish []int, days int) int {
-	currentFish := &initialFish
-	// fmt.Println("initial fish:", initialFish)
+	fishMap := createFishMap(initialFish)
+
 	for i := 0; i < days; i++ {
-		currentFish = SimulateDay(currentFish)
+		fishMap = SimulateDay(fishMap)
 		// fmt.Printf("Day %d: %v\n", i+1, *currentFish)
 	}
-	return len(*currentFish)
+
+	var total int
+	for i := 0; i < 9; i++ {
+		total += fishMap[i]
+	}
+
+	return total
 }
 
 func Run() {
-	days := 80
-	data := utils.LoadFile("./day_06_1/testInput.txt")
-	// data := utils.LoadFile("./day_06_1/input.txt")
+	days := 256
+	// data := utils.LoadFile("./day_06_1/testInput.txt")
+	data := utils.LoadFile("./day_06_1/input.txt")
 	fishStrings := strings.Split(data[0], ",")
 	initialFish := utils.MapStringsToInts(fishStrings)
 	fishCollection := make([]int, 0, len(initialFish)*len(initialFish))
--- day_06_1/main_test.go
+++ day_06_1/main_test.go
@@ -19,21 +19,21 @@ func TestGetFishCount(t *testing.T) {
 }
 
 func TestSimulateDayWithoutBirths(t *testing.T) {
-	initialFish := []int{3, 4, 3, 1, 2}
-	expectedFish := []int{2, 3, 2, 0, 1}
-	actualFish := day_06_1.SimulateDay(&initialFish)
+	initialFish := map[int]int{0: 1, 1: 1, 2: 1, 3: 2, 4: 1, 5: 0, 6: 0, 7: 0, 8: 0}
+	expectedFish := map[int]int{0: 1, 1: 1, 2: 2, 3: 1, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+	actualFish := day_06_1.SimulateDay(initialFish)
 
-	if !reflect.DeepEqual(expectedFish, *actualFish) {
+	if !reflect.DeepEqual(expectedFish, actualFish) {
 		t.Errorf("Incorrect fish! Expected %v but was %v", expectedFish, actualFish)
 	}
 }
 
 func TestSimulateDayWithBirths(t *testing.T) {
-	initialFish := []int{2, 3, 2, 0, 1}
-	expectedFish := []int{1, 2, 1, 6, 0, 8}
-	actualFish := day_06_1.SimulateDay(&initialFish)
+	initialFish := map[int]int{0: 1, 1: 1, 2: 2, 3: 1, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+	expectedFish := map[int]int{0: 1, 1: 2, 2: 1, 3: 0, 4: 0, 5: 0, 6: 1, 7: 0, 8: 1}
+	actualFish := day_06_1.SimulateDay(initialFish)
 
-	if !reflect.DeepEqual(expectedFish, *actualFish) {
+	if !reflect.DeepEqual(expectedFish, actualFish) {
 		t.Errorf("Incorrect fish! Expected %v but was %v", expectedFish, actualFish)
 	}
 }
