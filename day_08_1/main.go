521c0913d61a0b3549177159c0e67bbf4e948310
P. Michael Holland <maikeru.horando@gmail.com>
2022-01-02 17:25:55 +0100

    Day 8 part 2 Not as clean as I'd like but it got the job done.

--- day_08_1/main.go
+++ day_08_1/main.go
@@ -7,6 +7,11 @@ import (
 	"github.com/maikeru/adventofcode2021/utils"
 )
 
+type Entry struct {
+	UniquePatterns []string
+	Outputs        []string
+}
+
 func isEasyNumber(entry string) bool {
 	size := len(entry)
 	switch size {
@@ -41,10 +46,192 @@ func GetOutputs(data []string) [][]string {
 	return outputs
 }
 
+type filterFunction func(item string) bool
+
+func filterStrings(input []string, filterFunc filterFunction) []string {
+	filtered := make([]string, 0)
+	for i := range input {
+		item := input[i]
+		if filterFunc(item) {
+			filtered = append(filtered, item)
+		}
+	}
+	return filtered
+}
+
+func FindPatternsWithLength(patterns []string, length int) []string {
+	return filterStrings(patterns, func(item string) bool { return len(item) == length })
+}
+
+func findPatternDifference(p1 string, p2 string) []string {
+	longestPattern := p1
+	shortestPattern := p2
+	if len(p2) > len(p1) {
+		longestPattern = p2
+		shortestPattern = p1
+	}
+
+	difference := make([]string, 0)
+	for i := range longestPattern {
+		if !strings.Contains(shortestPattern, string(longestPattern[i])) {
+			difference = append(difference, string(longestPattern[i]))
+		}
+	}
+	return difference
+}
+
+/*
+Filters out patterns that don't contain everything in the search pattern (no partial matches)
+*/
+func FindPatternsContaining(patterns []string, search string) []string {
+	return filterStrings(patterns, func(pattern string) bool {
+		searchChars := strings.Split(search, "")
+		for j := range searchChars {
+			searchChar := searchChars[j]
+			if !strings.Contains(pattern, searchChar) {
+				return false
+			}
+		}
+
+		return true
+	})
+}
+
+/*
+ Filters out patterns that contain everything in the search pattern (no partial matches)
+*/
+func FindPatternsNotContaining(patterns []string, search string) []string {
+	return filterStrings(patterns, func(pattern string) bool {
+		searchChars := strings.Split(search, "")
+		// want to only match when the whole pattern matches
+		matchCount := 0
+		for j := range searchChars {
+			searchChar := searchChars[j]
+			if strings.Contains(pattern, searchChar) {
+				matchCount++
+			}
+		}
+		return matchCount < len(searchChars)
+	})
+}
+
+/*
+ Filter out patterns in the filter array from the input array. Exact matches only
+*/
+func FilterWholePatterns(input []string, filter []string) []string {
+	return filterStrings(input, func(pattern string) bool {
+		matches := false
+		for i := range filter {
+			filterPattern := filter[i]
+			if pattern == filterPattern {
+				matches = true
+			}
+		}
+		return !matches
+	})
+}
+
+func generateDecodeMap(uniquePatterns []string) map[string]string {
+	decodeMap := make(map[string]string)
+
+	sevenPattern := FindPatternsWithLength(uniquePatterns, 3)[0]
+	decodeMap[sevenPattern] = "7"
+	onePattern := FindPatternsWithLength(uniquePatterns, 2)[0]
+	decodeMap[onePattern] = "1"
+	fourPattern := FindPatternsWithLength(uniquePatterns, 4)[0]
+	decodeMap[fourPattern] = "4"
+	eightPattern := FindPatternsWithLength(uniquePatterns, 7)[0]
+	decodeMap[eightPattern] = "8"
+
+	// All the patterns with five segments
+	twoThreeFivePatterns := FindPatternsWithLength(uniquePatterns, 5)
+
+	// Only three contains both of the segments that exist in one
+	threePattern := FindPatternsContaining(twoThreeFivePatterns, onePattern)[0]
+	decodeMap[threePattern] = "3"
+
+	// Subtracting the pattern for four from eight gives us a "bottom-left" pattern
+	// That pattern exists in the pattern for two but not for five so should let us
+	// distinguish the remaining numbers
+	bottomLeftPattern := strings.Join(findPatternDifference(eightPattern, fourPattern), "")
+	twoPattern := FindPatternsContaining(twoThreeFivePatterns, bottomLeftPattern)[0]
+	decodeMap[twoPattern] = "2"
+	fivePattern := FilterWholePatterns(twoThreeFivePatterns, []string{twoPattern, threePattern})[0]
+	decodeMap[fivePattern] = "5"
+
+	// All the patterns with six segments
+	zeroSixNinePatterns := FindPatternsWithLength(uniquePatterns, 6)
+	// Only six does not contain the segments that exist in one out of zero, six and nine
+	sixPattern := FindPatternsNotContaining(zeroSixNinePatterns, onePattern)[0]
+	decodeMap[sixPattern] = "6"
+	// Nine contains three
+	ninePattern := FindPatternsContaining(zeroSixNinePatterns, threePattern)[0]
+	decodeMap[ninePattern] = "9"
+	zeroPattern := FilterWholePatterns(zeroSixNinePatterns, []string{sixPattern, ninePattern})[0]
+	decodeMap[zeroPattern] = "0"
+	return decodeMap
+}
+
+/*
+ Check if the two strings have the same characters but not necessarily in the same order
+*/
+func DoesPatternMatch(pattern string, toMatch string) bool {
+	if len(pattern) != len(toMatch) {
+		return false
+	}
+
+	for i := range toMatch {
+		characterToMatch := string(toMatch[i])
+		if !strings.Contains(pattern, characterToMatch) {
+			return false
+		}
+	}
+	return true
+}
+
+func decodeItem(decodeMap map[string]string, item string) string {
+	for pattern, value := range decodeMap {
+		if DoesPatternMatch(pattern, item) {
+			return value
+		}
+	}
+	panic(fmt.Sprintf("Unable to decode item %s", item))
+}
+
+func DecodeLine(entry Entry) int {
+	decodeMap := generateDecodeMap(entry.UniquePatterns)
+	decodedOutputs := ""
+	for i := range entry.Outputs {
+		output := entry.Outputs[i]
+		decodedOutputs += decodeItem(decodeMap, output)
+	}
+	return utils.StringToInt(decodedOutputs)
+}
+
+func toEntry(line string) Entry {
+	patternsAndOutputs := strings.Split(line, " | ")
+	uniquePatterns := strings.Split(patternsAndOutputs[0], " ")
+	outputs := strings.Split(patternsAndOutputs[1], " ")
+	return Entry{UniquePatterns: uniquePatterns, Outputs: outputs}
+}
+
+func DecodeAndSumLines(data []string) int {
+	total := 0
+	for i := range data {
+		line := data[i]
+		entry := toEntry(line)
+		value := DecodeLine(entry)
+		total += value
+	}
+	return total
+}
+
 func Run() {
 	// data := utils.LoadFile("./day_08_1/testInput.txt")
 	data := utils.LoadFile("./day_08_1/input.txt")
 	outputs := GetOutputs(data)
 	count := CountEasyNumbersInOutput(outputs)
 	fmt.Println("PART 1: count of easy numbers", count)
+	total := DecodeAndSumLines(data)
+	fmt.Println("PART 2: total of all outputs", total)
 }
--- day_08_1/main_test.go
+++ day_08_1/main_test.go
@@ -1,6 +1,7 @@
 package day_08_1_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/maikeru/adventofcode2021/day_08_1"
@@ -15,6 +16,83 @@ func TestCountEasyNumbersInOutput(t *testing.T) {
 
 	if count != expectedCount {
 		t.Errorf("Incorrect count! Expected %d but got %d", expectedCount, count)
+	}
+}
+
+func TestFindPatternsWithLength(t *testing.T) {
+	patterns := []string{"abc", "bc", "defg", "b"}
+	expectedPatterns := []string{"bc"}
+	actualPatterns := day_08_1.FindPatternsWithLength(patterns, 2)
+	if !reflect.DeepEqual(actualPatterns, expectedPatterns) {
+		t.Errorf("Did not find patterns with length 2! Expected %v but got %v", expectedPatterns, actualPatterns)
+	}
+}
+
+func TestFindPatternsContaining(t *testing.T) {
+	patterns := []string{"abc", "bc", "defg", "b"}
+	search := "bc"
+	expectedPatterns := []string{"abc", "bc"}
+	actualPatterns := day_08_1.FindPatternsContaining(patterns, search)
+	if !reflect.DeepEqual(expectedPatterns, actualPatterns) {
+		t.Errorf("Incorrect patterns! Expected %v but got %v", expectedPatterns, actualPatterns)
+	}
+}
+
+func TestFindPatternsNotContaining(t *testing.T) {
+	patterns := []string{"abc", "bc", "defg", "b"}
+	search := "bc"
+	expectedPatterns := []string{"defg", "b"}
+	actualPatterns := day_08_1.FindPatternsNotContaining(patterns, search)
+	if !reflect.DeepEqual(expectedPatterns, actualPatterns) {
+		t.Errorf("Incorrect patterns! Expected %v but got %v", expectedPatterns, actualPatterns)
+	}
+}
+
+func TestFilterWholePatterns(t *testing.T) {
+	patterns := []string{"abc", "bc", "defg", "b"}
+	search := []string{"bc", "defg"}
+	expectedPatterns := []string{"abc", "b"}
+	actualPatterns := day_08_1.FilterWholePatterns(patterns, search)
+	if !reflect.DeepEqual(expectedPatterns, actualPatterns) {
+		t.Errorf("Incorrect patterns! Expected %v but got %v", expectedPatterns, actualPatterns)
+	}
+}
+
+func TestDoesPatternMatchWhenShouldMatch(t *testing.T) {
+	pattern := "cba"
+	toMatch := "abc"
+	if !day_08_1.DoesPatternMatch(pattern, toMatch) {
+		t.Errorf("Pattern should match but didn't! pattern: %s toMatch: %s", pattern, toMatch)
+	}
+}
+
+func TestDoesPatternMatchWhenInputsAreDifferentLengths(t *testing.T) {
+	pattern := "cb"
+	toMatch := "abc"
+	if day_08_1.DoesPatternMatch(pattern, toMatch) {
+		t.Errorf("Pattern should not match but did! pattern: %s toMatch: %s", pattern, toMatch)
+	}
+}
+
+func TestDoesPatternMatchWhenInputsAreSameLengths(t *testing.T) {
+	pattern := "cbe"
+	toMatch := "abc"
+	if day_08_1.DoesPatternMatch(pattern, toMatch) {
+		t.Errorf("Pattern should not match but did! pattern: %s toMatch: %s", pattern, toMatch)
+	}
+}
+
+func TestDecodeLine(t *testing.T) {
+	line := day_08_1.Entry{
+		UniquePatterns: []string{"acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"},
+		Outputs:        []string{"cdfeb", "fcadb", "cdfeb", "cdbaf"},
+	}
+
+	expectedOutput := 5353
+	output := day_08_1.DecodeLine(line)
+
+	if output != expectedOutput {
+		t.Errorf("Incorrect output! Expected [%d] but got [%d]", expectedOutput, output)
 	}
 
 }
